8364306bd7f3a4bb0afbf61f5e5b473db75d45b2
sina <cena.tabriziyan@gmail.com>
2020-09-21 21:30:34 +0330

    create app

---
+++ app/src/main/java/com/android_devs/news/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.android_devs.news.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.android_devs.news.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+}
---
+++ app/src/main/java/com/android_devs/news/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.android_devs.news.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.android_devs.news.R
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+}

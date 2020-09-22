23731d9af65e97241ea8ff653be492e847079428
sina <cena.tabriziyan@gmail.com>
2020-09-22 18:53:31 +0330

    Implement and Observe breakingNews in ViewModel

--- app/src/main/java/com/android_devs/news/ui/NewsViewModel.kt
+++ app/src/main/java/com/android_devs/news/ui/NewsViewModel.kt
@@ -1,8 +1,36 @@
 package com.android_devs.news.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android_devs.news.models.NewsResponse
 import com.android_devs.news.repository.NewsRepository
+import com.android_devs.news.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
 
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }

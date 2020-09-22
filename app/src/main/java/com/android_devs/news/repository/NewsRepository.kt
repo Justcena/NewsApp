f79d25d2e59d12bba332a3edbce76ccbcb412bb6
sina <cena.tabriziyan@gmail.com>
2020-09-22 18:49:39 +0330

    Create News Response

--- app/src/main/java/com/android_devs/news/repository/NewsRepository.kt
+++ app/src/main/java/com/android_devs/news/repository/NewsRepository.kt
@@ -1,6 +1,12 @@
 package com.android_devs.news.repository
 
+import com.android_devs.news.api.RetrofitInstance
 import com.android_devs.news.db.ArticleDatabase
 
 class NewsRepository(val database: ArticleDatabase) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+
 }
